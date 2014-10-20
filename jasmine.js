d478e4ecbb3f8f6ea2b090c1a35048a0a4d95319
Is NASDAQ open <isnasdaqopen@users.noreply.github.com>
2014-10-20 22:57:59 +0300

    Refactored

--- jasmine.js
+++ jasmine.js
@@ -5,10 +5,12 @@ describe("Timezone: EST", function () {
         expect(result.timeUntilNextStatus).toBe('0 hours 1 minutes 0 seconds');
     });
     it("Friday 09:30", function () {
-        expect(nasdaqStatus('2014-01-24T09:30:00-05:00').currentStatus).toBe('OPEN');
+        var result = nasdaqStatus('2014-01-24T09:30:00-05:00');
+        expect(result.currentStatus).toBe('OPEN');
     });
     it("Friday 16:00", function () {
-        expect(nasdaqStatus('2014-01-24T16:00:00-05:00').currentStatus).toBe('OPEN');
+        var result = nasdaqStatus('2014-01-24T16:00:00-05:00');
+        expect(result.currentStatus).toBe('OPEN');
     });
     it("Friday 16:01", function () {
         var result = nasdaqStatus('2014-01-24T16:01:00-05:00');
@@ -16,46 +18,58 @@ describe("Timezone: EST", function () {
         expect(result.timeUntilNextStatus).toBe('17 hours 29 minutes 0 seconds');
     });
     it("Saturday", function () {
-        expect(nasdaqStatus('2014-01-26T10:00:00-05:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-01-26T10:00:00-05:00');
+        expect(result.currentStatus).toBe('CLOSED');
         //expect(result.timeUntilNextStatus).toBe('24*2-0.5 hours 0 minutes');
     });
     it("Sunday", function () {
-        expect(nasdaqStatus('2014-01-26T10:00:00-05:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-01-26T10:00:00-05:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
 });
 
 describe("Timezone: EET", function () {
     it("Sunday", function () {
-        expect(nasdaqStatus('2014-01-26T10:00:00+02:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-01-26T10:00:00+02:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
     it("Monday 10:00", function () {
-        expect(nasdaqStatus('2014-01-27T10:00:00+02:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-01-27T10:00:00+02:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
     it("Monday 22:59:59", function () {
-        expect(nasdaqStatus('2014-01-27T22:59:59+02:00').currentStatus).toBe('OPEN');
+        var result = nasdaqStatus('2014-01-27T22:59:59+02:00');
+        expect(result.currentStatus).toBe('OPEN');
     });
     it("Monday 23:00:00", function () {
-        expect(nasdaqStatus('2014-01-27T23:00:00+02:00').currentStatus).toBe('OPEN');
+        var result = nasdaqStatus('2014-01-27T23:00:00+02:00');
+        expect(result.currentStatus).toBe('OPEN');
     });
     it("Monday 23:00:01", function () {
-        expect(nasdaqStatus('2014-01-27T23:00:01+02:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-01-27T23:00:01+02:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
 });
 
 describe("Timezone: EET, summer time", function () {
     it("Sunday", function () {
-        expect(nasdaqStatus('2014-10-14T10:00:00+03:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-10-14T10:00:00+03:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
     it("Monday 10:00", function () {
-        expect(nasdaqStatus('2014-10-15T10:00:00+03:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-10-15T10:00:00+03:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
     it("Monday 22:59:59", function () {
-        expect(nasdaqStatus('2014-10-15T22:59:59+03:00').currentStatus).toBe('OPEN');
+        var result = nasdaqStatus('2014-10-15T22:59:59+03:00');
+        expect(result.currentStatus).toBe('OPEN');
     });
     it("Monday 23:00:00", function () {
-        expect(nasdaqStatus('2014-10-15T23:00:00+03:00').currentStatus).toBe('OPEN');
+        var result = nasdaqStatus('2014-10-15T23:00:00+03:00');
+        expect(result.currentStatus).toBe('OPEN');
     });
     it("Monday 23:00:01", function () {
-        expect(nasdaqStatus('2014-10-15T23:00:01+03:00').currentStatus).toBe('CLOSED');
+        var result = nasdaqStatus('2014-10-15T23:00:01+03:00');
+        expect(result.currentStatus).toBe('CLOSED');
     });
 });
