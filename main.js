c678effd761bb1dbd78df0baa99b1d098c09483a
Is NASDAQ open <isnasdaqopen@users.noreply.github.com>
2014-10-20 21:46:32 +0300

    Refactored

--- main.js
+++ main.js
@@ -55,40 +55,30 @@ function myTimer() {
     var status = nasdaqStatus();
     $('#status').html(status.currentStatus);
     //$('#timeUntilNextStatus').html(status.timeUntilNextStatus);
+    countdown()
 }
-
-myTimer();
-
 
 //set the date we're counting down to
 var utcToEsternTimeOffsetHours = 4;
 var targetDate = new Date(Date.UTC(2014, 10-1, 20, 16 + utcToEsternTimeOffsetHours, 00)).getTime();
- 
-// variables for time units
-var days, hours, minutes, seconds;
- 
-// get tag element
-var countdown = document.getElementById("timeUntilNextStatus");
- 
-// update the tag with id "countdown" every 1 second
-setInterval(function () {
- 
+
+function countdown() {
     // find the amount of "seconds" between now and target
     var currentDate = new Date().getTime();
     var secondsLeft = (targetDate - currentDate) / 1000;
  
     // do some time calculations
-    days = parseInt(secondsLeft / 86400);
+    var days = parseInt(secondsLeft / 86400);
     secondsLeft = secondsLeft % 86400;
      
-    hours = parseInt(secondsLeft / 3600);
+    var hours = parseInt(secondsLeft / 3600);
     secondsLeft = secondsLeft % 3600;
      
-    minutes = parseInt(secondsLeft / 60);
-    seconds = parseInt(secondsLeft % 60);
+    var minutes = parseInt(secondsLeft / 60);
+    var seconds = parseInt(secondsLeft % 60);
      
-    // format countdown string + set tag value
     $('#timeUntilNextStatus').html(days + "d " + hours + "h "
     	    + minutes + "m " + seconds + "s");
- 
-}, 1000);
+}
+
+setInterval(myTimer, 1000);
